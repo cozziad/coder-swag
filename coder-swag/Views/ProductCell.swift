c290d8f0f5ba760224830f0643222d6e8c53f16e
Anthony <cozziad@gmail.com>
2019-06-26 19:40:17 -0400

    Lecture 58 Done

---
+++ coder-swag/Views/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Anthony Cozzi on 6/26/19.
+//  Copyright © 2019 Anthony Cozzi. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productName: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
