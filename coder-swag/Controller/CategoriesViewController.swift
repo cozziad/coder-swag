eef423dcf5218e04be9ca79427aaa6ea31ed0446
Anthony <cozziad@gmail.com>
2019-06-26 09:45:18 -0400

    Lecture 56 Done

--- coder-swag/Controller/CategoriesViewController.swift
+++ coder-swag/Controller/CategoriesViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class CategoriesViewController: UIViewController {
 
+    @IBOutlet weak var categoryTable: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
